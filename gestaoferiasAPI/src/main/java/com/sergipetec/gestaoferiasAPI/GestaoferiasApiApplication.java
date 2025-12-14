b7b3a7134ac33ea6ee094dc0d67f7b3358f14a9c
Joanne PC casa <joannestephany@gmail.com>
2025-12-14 00:56:56 -0300

    Feat::  Main Application

---
+++ gestaoferiasAPI/src/main/java/com/sergipetec/gestaoferiasAPI/GestaoferiasApiApplication.java
@@ -0,0 +1,13 @@
+package com.sergipetec.gestaoferiasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoferiasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoferiasApiApplication.class, args);
+	}
+
+}
