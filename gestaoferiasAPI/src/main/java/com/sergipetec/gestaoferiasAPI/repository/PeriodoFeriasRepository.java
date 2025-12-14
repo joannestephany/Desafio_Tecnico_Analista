a46cbf437e37362e2a79ffc0c435ccd62ebc50a5
Joanne PC casa <joannestephany@gmail.com>
2025-12-14 00:49:04 -0300

    Feat:: repository for data access
    Camada de acesso aos dados

---
+++ gestaoferiasAPI/src/main/java/com/sergipetec/gestaoferiasAPI/repository/PeriodoFeriasRepository.java
@@ -0,0 +1,12 @@
+package com.sergipetec.gestaoferiasAPI.repository;
+
+import com.sergipetec.gestaoferiasAPI.entity.PeriodoFerias;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PeriodoFeriasRepository
+        extends JpaRepository<PeriodoFerias, Long> {
+
+    List<PeriodoFerias> findByServidorId(Long servidorId);
+}
