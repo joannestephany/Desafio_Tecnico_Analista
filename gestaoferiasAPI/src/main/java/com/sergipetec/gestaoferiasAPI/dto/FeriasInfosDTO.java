e706127aae3120b59c5d2c35e22a27c043e431d4
Joanne PC casa <joannestephany@gmail.com>
2025-12-14 19:52:16 -0300

    Update:: DTOs StatusSolicitacao enum
    Refactors the FeriasInfosDTO and FeriasResumoDTO to use the StatusSolicitacao enum instead of a String for representing the status of a vacation request.
    
    This change improves type safety and code maintainability by enforcing a predefined set of status values.

--- gestaoferiasAPI/src/main/java/com/sergipetec/gestaoferiasAPI/dto/FeriasInfosDTO.java
+++ gestaoferiasAPI/src/main/java/com/sergipetec/gestaoferiasAPI/dto/FeriasInfosDTO.java
@@ -2,13 +2,14 @@ package com.sergipetec.gestaoferiasAPI.dto;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import com.sergipetec.gestaoferiasAPI.entity.StatusSolicitacao;
 
 public record FeriasInfosDTO(
-        Long id,
-        LocalDate dataInicio,
-        LocalDate dataFim,
-        String status,
-        BigDecimal valorBruto,
-        BigDecimal valorLiquido,
-        LocalDate dataPagamento
-) {}
+                Long id,
+                LocalDate dataInicio,
+                LocalDate dataFim,
+                StatusSolicitacao status,
+                BigDecimal valorBruto,
+                BigDecimal valorLiquido,
+                LocalDate dataPagamento) {
+}
--- gestaoferiasAPI/src/main/java/com/sergipetec/gestaoferiasAPI/dto/FeriasResumoDTO.java
+++ gestaoferiasAPI/src/main/java/com/sergipetec/gestaoferiasAPI/dto/FeriasResumoDTO.java
@@ -1,10 +1,11 @@
 package com.sergipetec.gestaoferiasAPI.dto;
 
 import java.time.LocalDate;
+import com.sergipetec.gestaoferiasAPI.entity.StatusSolicitacao;
 
 public record FeriasResumoDTO(
-        Long id,
-        LocalDate dataInicio,
-        LocalDate dataFim,
-        String status
-) {}
+                Long id,
+                LocalDate dataInicio,
+                LocalDate dataFim,
+                StatusSolicitacao status) {
+}
